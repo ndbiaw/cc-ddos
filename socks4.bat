ca8c27e53f0b76d774618a7aa257ca170e1b10a4
Ling <59746573+ndbiaw@users.noreply.github.com>
2022-08-29 06:39:33 +0700

    Cập nhật script tự động trên Linux, thêm source proxy

--- socks4.bat
+++ socks4.bat
@@ -1,3 +1,3 @@
 @echo off
 del socks4.txt
-python cc.py -url %SETURL% -m cc -v 4 -t 1000 -f socks4.txt -b 1 -s 999999999999999 -down -check
+python cc.py -url %SETURL% -m cc -v 4 -t 1000 -f socks4.txt -b 1 -s 9223372036 -down -check
