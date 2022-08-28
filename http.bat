6d87cb4eed1ba73c3d61e20d28aa66eb5fc5b6db
Ling <59746573+ndbiaw@users.noreply.github.com>
2022-08-29 06:39:40 +0700

    Cập nhật script tự động trên Linux, thêm source proxy

--- http.bat
+++ http.bat
@@ -1,3 +1,3 @@
 @echo off
 del http.txt
-python cc.py -url %SETURL% -m cc -v http -t 1000 -f http.txt -b 1 -s 999999999999999 -down -check
+python cc.py -url %SETURL% -m cc -v http -t 1000 -f http.txt -b 1 -s 9223372036 -down -check
