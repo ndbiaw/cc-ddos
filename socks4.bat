e9b273fa53239f7b5d713a01734cf6391e1b55fc
Ling <59746573+ndbiaw@users.noreply.github.com>
2022-09-02 06:06:21 +0700

    -down -check

--- socks4.bat
+++ socks4.bat
@@ -1,3 +1,3 @@
 @echo off
 del socks4.txt
-python cc.py -url %SETURL% -m cc -v 4 -t 1000 -f %PROXY%socks4.txt -b 1 -s 2592000 -down %VERIFY%
+python cc.py -url %SETURL% -m cc -v 4 -t 1000 -f %PROXY%socks4.txt -b 1 -s 2592000 %VERIFY%
