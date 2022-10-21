ad88ecec7885835e3042af6747b55390d1d1a2f1
Ling <59746573+ndbiaw@users.noreply.github.com>
2022-10-21 14:20:11 +0700

    Update automate linux shell

--- start_proxy.bat
+++ start_proxy.bat
@@ -1,9 +1,9 @@
 @echo off
-python proxy.py
 echo Nhan Enter de tiep tuc. Sau khi an Enter se khoi chay 3 quy trinh tan cong DDoS CC cung luc. De co tuy chon khac, vui long su sung Termial.
 set /p SETURL="Nhap URL trang web can tan cong: "
 set PROXY=.\proxies\
 set VERIFY=
+pip3 install -r requirements.txt
 python proxy.py
 pause
 start socks5.bat
