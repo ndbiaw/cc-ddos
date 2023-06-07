90a98c798070b5f6a099261b34018e45abf353a7
Ling <nguyenduybachreal@gmail.com>
2023-06-07 15:45:08 +0700

    more streamlined

--- socks4.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -rf socks4.txt
-python3 cc.py -url $URL -m cc -v 4 -t 1000 -f $PROXYsocks4.txt -b 1 -s 2592000 $VERIFY
--- socks5.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -rf socks5.txt
-python3 cc.py -url $URL -m cc -v 5 -t 1000 -f $PROXYsocks5.txt -b 1 -s 2592000 $VERIFY
